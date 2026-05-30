690b0fa32d61bebddee9e16218fbfe3f036e41ef
aesd <aesd@Ubuntu.myguest.virtualbox.org>
2026-05-30 18:24:15 -0400

    use master branch for aesd assignments package

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AESD_ASSIGNMENTS_VERSION = 180956e6a7f7348a51dba868b2ed40b5389cefe2
+AESD_ASSIGNMENTS_VERSION = master
 AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignment-4-emlinuxst.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
