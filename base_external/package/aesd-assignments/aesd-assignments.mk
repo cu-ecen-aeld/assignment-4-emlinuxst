50febc211d42a592c5857d0eb8fdc0ec2647133b
aesd <aesd@Ubuntu.myguest.virtualbox.org>
2026-05-30 12:24:14 -0400

    fix full test requirements

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 AESD_ASSIGNMENTS_VERSION = 735baae5ac7ecba2716f7ad41eb79f2d8d60f03a
-AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-emlinuxst.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-emlinuxst.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
